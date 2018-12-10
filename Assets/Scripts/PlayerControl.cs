50c59372ab28f1b8fff0dafd40c2ee4cb3eb0141
Ravi Patel <ravipatelnov1@gmail.com>
2018-12-10 12:10:41 -0500

    mjk

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerControl : MonoBehaviour {
+	public float speed;
+	private Rigidbody rb;
+	private int count;
+	public Text countText;
+	public Text wintext;
+	public float disToGround = 0.5f;
+	Vector3 Startpos;
+	void Start (){
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		//SetCountText();
+		wintext.text = "";
+		Startpos = transform.position;
+	}
+
+	void FixedUpdate(){
+
+		//Debug.Log (isGrounded());
+		if (Input.GetKeyDown ("r")) {
+			transform.position = Startpos;
+		}
+
+		if (Input.GetKey(KeyCode.Space)&& isGrounded()) {
+			rb.AddForce (0, 500f, 0);
+		}
+
+
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce (movement * speed);
+	}
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText();
+		}
+	}
+	void SetCountText(){
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			wintext.text = "You Win";
+		}
+	}
+	bool isGrounded(){
+		return Physics.Raycast (transform.position, Vector3.down, disToGround);
+
+	
+	}
+}
+
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+//	Vector3 pointA = new Vector3(transform.position.x,transform.position.y,transform.position.z);
+//	Vector3 pointB = new Vector3 (transform.position.x,transform.position.y,transform.position.z);
+
+
+	void Update(){
+		
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+//		transform.position = Vector3.Lerp(pointA, pointB, Mathf.PingPong(Time.time, 1));
+
+	}
+}
